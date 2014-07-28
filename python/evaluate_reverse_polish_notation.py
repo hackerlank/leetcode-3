0811da0521ebb11cb75f3a7a5b464fb664d7a6a9
yummybian <yummy.bian@gmail.com>
2014-07-28 13:21:40 +0800

    Create evaluate_reverse_polish_notation.py

---
+++ python/evaluate_reverse_polish_notation.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @param tokens, a list of string
+    # @return an integer
+    def evalRPN(self, tokens):
+        import operator
+        ops = {
+            '+': operator.add,
+            '-': operator.sub,
+            '*': operator.mul,
+            '/': operator.div 
+        }
+        stack = []
+        for token in tokens:
+            if token not in ops:
+                stack.append(int(token))
+            else:
+                y, x = stack.pop(), stack.pop()
+                stack.append(ops[token](x, y))
+                
+        return stack
