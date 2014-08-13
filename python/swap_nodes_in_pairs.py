f59e2b5c9d3c67297090e2c3bac9815f7dcb200e
yummybian <yummy.bian@gmail.com>
2014-08-13 13:05:26 +0800

    Create swap_nodes_in_pairs.py

---
+++ python/swap_nodes_in_pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param a ListNode
+    # @return a ListNode
+    def swapPairs(self, head):
+        if head != None and head.next != None:
+            next = head.next
+            head.next = self.swapPairs(next.next)
+            next.next = head
+            return next
+        return head
